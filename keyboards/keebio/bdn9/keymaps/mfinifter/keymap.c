70f5cf406f11f40d3d044522e980c9e3067a3a04
msf <msf@matthewfinifter.com>
2021-12-21 15:01:29 -0800

    key mappings but not yet done with encoders for BDN9

--- keyboards/keebio/bdn9/keymaps/mfinifter/keymap.c
+++ keyboards/keebio/bdn9/keymaps/mfinifter/keymap.c
@@ -22,52 +22,68 @@ enum encoder_names {
 };
 
 // Ideas:
-// * window management / desktop switching / application switching
-// * zoom mute / face mute
-// * web browser: scrolling, changing tabs, nav back/forward
-// /
 // https://www.reddit.com/r/olkb/comments/bauj0t/can_you_change_encoders_output_using_layers_qmk/
 // talks about making encoders do different things on different layers
 //
 // https://docs.splitkb.com/hc/en-us/articles/360010513760-How-can-I-use-a-rotary-encoder- talks
 // about setting up command-tabbing for application switching
 //
-// all layers have a key to switch to layer 0, layer 1, layer 2
-//
-// layer 0: navigation, web browsing, and VC
-//   encoder 0:
-//   encoder 1:
-//   encoder 2:
-// layer 1: application management
-//   encoder 0: virtual desktop switching
-//   encoder 1: app switching 
-//   encoder 2: tab switching
-// layer 2: window management
+// layer 0: window management
 //   encoder 0: shrink/grow master pane
 //     press: cycle layout
 //   encoder 1: select active window
+//     press: move active window to master pane
 //   encoder 2: move active window counter-clockwise / clockwise
+//     press: move active window to other screen
+//   minimize / full-screen toggle / close window
+//   hold for adjust layer / go to layer 1 / go to layer 2
+// layer 1: navigation, web browsing, and zoom
+//   encoder 0: mouse wheel up/down
+//     press: home
+//   encoder 1: mouse wheel left/right
+//     press: end
+//   encoder 2: browser tab left/right
+//     press: close tab
+//   mute / face mute / start/stop screen share
+//   go to layer 0 / nothing / go to layer 2
+// layer 2: text editing
+//   encoder 0: scroll word at a time
+//     press: cut
+//   encoder 1: left/right
+//     press: copy
+//   encoder 2: up/down
+//     press: paste
+//   shift/backspace/delete
+//   go to layer 0 / go to layer 1 / nothing
+// layer ADJUST: reset / rgb control
+//   encoder 0: increase/decrease hue
+//    press: toggle lighting on/off
+//   encoder 1: increase/decrease saturation
+//    press: cycle through modes forward
+//   encoder 2: increase/decrease brightness
+//    press: cycle through modes backward
+//   bl toggle / bl step / bl breathing
+//   nothing / nothing / reset
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-        | Knob 1: Vol Dn/Up |      | Knob 2: Page Dn/Up |
-        | Press: Mute       | Home | Press: Play/Pause  |
-        | Hold: Layer 2     | Up   | RGB Mode           |
-        | Left              | Down | Right              |
-     */
     [0] = LAYOUT(
-        KC_MUTE, KC_HOME, KC_MPLY,
-        MO(1)  , KC_UP  , RGB_MOD,
-        KC_LEFT, KC_DOWN, KC_RGHT
+        LALT(KC_SPACE), LSA(KC_ENTER), MEH(KC_J),
+        LGUI(KC_M), LGUI(LCTL(KC_F)), LGUI(KC_W),
+        MO(3), TO(1), TO(2)
     ),
-    /*
-        | RESET          | N/A  | Media Stop |
-        | Held: Layer 2  | Home | RGB Mode   |
-        | Media Previous | End  | Media Next |
-     */
     [1] = LAYOUT(
-        RESET  , BL_STEP, KC_STOP,
-        _______, KC_HOME, RGB_MOD,
-        KC_MPRV, KC_END , KC_MNXT
+        KC_HOME, KC_END, LGUI(KC_W),
+        LSG(KC_A), LSG(KC_V), LSG(KC_S),
+        TO(0), KC_TRNS, TO(2)
+    ),
+    [2] = LAYOUT(
+        LGUI(KC_X), LGUI(KC_C), LGUI(KC_V),
+        KC_LEFT_SHIFT, KC_BACKSPACE, KC_DELETE,
+        TO(0), TO(1), KC_TRNS
+    ),
+    [3] = LAYOUT(
+        RGB_TOG, RGB_MODE_FORWARD, RGB_MODE_REVERSE,
+        BL_TOGG, BL_STEP, BL_BRTG,
+        KC_TRNS, KC_TRNS, RESET
     ),
 };
 
